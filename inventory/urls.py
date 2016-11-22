fed6289ff03dd0afb7cb95b35938e35a203f7bce
Gregory Bird <Gregory Bird>
2016-11-22 16:14:01 -0700

    added add item page

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,5 @@ app_name = 'inventory'
 urlpatterns = [
     url(r'^index/$', views.view_inventory, name='index'),
     url(r'^delete/([0-9]+)/$', views.delete, name='delete_inventory'),
+    url(r'^add/$', views.add, name='add')
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Item
+from .models import Item, Location, Manufacturer
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required
+from django import forms
 
 # Create your views here.
 @login_required(login_url='/account/login/')
@@ -28,3 +29,39 @@ def delete(request, id):
     item.delete()
 
     return HttpResponseRedirect('/inventory/index/')
+
+@login_required(login_url='/account/login/')
+def add(request):
+	'''Adds an item to the inventory'''
+
+	form = AddItemForm()
+	if request.method == 'POST':
+		form = AddItemForm(request.POST)
+		if form.is_valid():
+			i = Item()
+			i.name = form.cleaned_data.get('name')
+			i.organizationalTag = form.cleaned_data.get('organizationalTag')
+			i.manufacturerPartNumber = form.cleaned_data.get('manufacturerPartNumber')
+			i.dateImplemented = form.cleaned_data.get('dateImplemented')
+			i.description = form.cleaned_data.get('description')
+			i.location = form.cleaned_data.get('location')
+			i.manufacturer = form.cleaned_data.get('manufacturer')
+
+			i.save()
+
+			return HttpResponseRedirect('/inventory/index')
+
+
+	template_vars = {
+		'form': form
+	}
+
+	return render(request, 'inventory/add.html', template_vars)
+class AddItemForm(forms.Form):
+	name = forms.CharField(label='Name', required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	organizationalTag = forms.CharField(label='Organizational Tag', required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	manufacturerPartNumber = forms.CharField(label='Manufacturer Part Number', required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	dateImplemented = forms.DateField(label='Date Implemented', widget=forms.TextInput(attrs={'class': 'form-control'}))
+	description = forms.CharField(label='Description', required=True, max_length=140, widget=forms.Textarea(attrs={'class': 'form-control'}))
+	location = forms.ModelChoiceField(label='Location', required=True, queryset=Location.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
+	manufacturer = forms.ModelChoiceField(label='Manufacturer', required=True, queryset=Manufacturer.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
