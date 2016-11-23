7ee8aece0eb635ebf35d9f70240a7698354737ff
Gregory Bird <Gregory Bird>
2016-11-22 19:13:54 -0700

    added edit item page

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^index/$', views.view_inventory, name='index'),
     url(r'^delete/([0-9]+)/$', views.delete, name='delete_inventory'),
     url(r'^add/$', views.add, name='add'),
+    url(r'^edit/([0-9]+)/$', views.editItem, name='editItem'),
     url(r'^notes/([0-9]+)/$', views.viewNotes, name='viewNotes'),
     url(r'^addNotes/([0-9]+)/$', views.addNotes, name='addNotes'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -68,6 +68,39 @@ class AddItemForm(forms.Form):
 	manufacturer = forms.ModelChoiceField(label='Manufacturer', required=True, queryset=Manufacturer.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
 
 @login_required(login_url='/account/login/')
+def editItem(request, itemId):
+	''' Edits an item'''
+	try:
+		item = Item.objects.get(id = itemId)
+	except Item.DoesNotExist:
+		return HttpResponseRedirect('/inventory/index/')
+
+	form = AddItemForm(initial={'name': item.name, 'organizationalTag': item.organizationalTag, 'manufacturerPartNumber': item.manufacturerPartNumber, 'dateImplemented': item.dateImplemented, 'description': item.description, 'location': item.location, 'manufacturer': item.manufacturer})
+
+	if request.method == 'POST':
+		form = AddItemForm(request.POST)
+		if form.is_valid():
+			i = Item.objects.get(id = itemId)
+			i.name = form.cleaned_data.get('name')
+			i.organizationalTag = form.cleaned_data.get('organizationalTag')
+			i.manufacturerPartNumber = form.cleaned_data.get('manufacturerPartNumber')
+			i.dateImplemented = form.cleaned_data.get('dateImplemented')
+			i.description = form.cleaned_data.get('description')
+			i.location = form.cleaned_data.get('location')
+			i.manufacturer = form.cleaned_data.get('manufacturer')
+
+			i.save()
+
+			return HttpResponseRedirect('/inventory/index')
+
+	template_vars = {
+		'form': form,
+		'item': item
+	}
+
+	return render(request, 'inventory/editItem.html', template_vars)
+
+@login_required(login_url='/account/login/')
 def viewNotes(request, itemId):
 	notes = Note.objects.all().filter(item = itemId)
 	item = Item.objects.get(id = itemId)
